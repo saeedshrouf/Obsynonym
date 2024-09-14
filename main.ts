3191e744e898f79f94bf04e814ec1ca717010f4b
Saeed Al-Shrouf <realsaeedshrouf@gmail.com>
2024-09-14 14:28:37 -0400

    Set up synonym code in main.ts, set up manifest.json

--- main.ts
+++ main.ts
@@ -1,14 +1,21 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
 
 interface MyPluginSettings {
 	mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	mySetting: "default",
+};
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -16,97 +23,107 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// Existing sample code (optional to keep)
+		// ...
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// Add the synonym suggestion command
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: "suggest-synonyms",
+			name: "Suggest Synonyms",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				const selectedText = editor.getSelection();
+				if (selectedText) {
+					this.suggestSynonyms(selectedText, editor);
+				} else {
+					new Notice("Please select a word to get synonyms.");
 				}
+			},
+		});
+
+		// Any other initialization code
+		// ...
+	}
+
+	async suggestSynonyms(word: string, editor: Editor) {
+		try {
+			const synonyms = await this.fetchSynonyms(word);
+			if (synonyms.length > 0) {
+				new SynonymModal(this.app, synonyms, (replacement) => {
+					editor.replaceSelection(replacement);
+				}).open();
+			} else {
+				new Notice("No synonyms found.");
 			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		} catch (error) {
+			new Notice("Error fetching synonyms.");
+			console.error(error);
+		}
 	}
 
-	onunload() {
-
+	async fetchSynonyms(word: string): Promise<string[]> {
+		const response = await fetch(
+			`https://api.datamuse.com/words?rel_syn=${encodeURIComponent(word)}`
+		);
+		const data = await response.json();
+		return data.map((item: any) => item.word);
 	}
 
+	// Existing methods for settings, unloading, etc.
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	onunload() {
+		// Cleanup code if needed
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+// SynonymModal class outside the plugin class
+class SynonymModal extends Modal {
+	synonyms: string[];
+	onChoose: (synonym: string) => void;
+
+	constructor(
+		app: App,
+		synonyms: string[],
+		onChoose: (synonym: string) => void
+	) {
 		super(app);
+		this.synonyms = synonyms;
+		this.onChoose = onChoose;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.createEl("h2", { text: "Select a Synonym" });
+
+		this.synonyms.forEach((synonym) => {
+			const synonymEl = contentEl.createEl("div", {
+				text: synonym,
+				cls: "synonym-item",
+			});
+			synonymEl.onclick = () => {
+				this.onChoose(synonym);
+				this.close();
+			};
+		});
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
+// Optionally, keep or update your SampleSettingTab and SampleModal classes
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
@@ -116,19 +133,21 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
